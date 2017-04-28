ad2e79396ca040c39551cab10ca37cb3be38f43d
Ben Corrie <bcorrie@vmware.com>
2017-04-28 05:05:28 +0000

    update build script to push latest

--- vic-machine/build/push.sh
+++ vic-machine/build/push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPO_NAME="bensdoings"
-VERSION="latest"
+VERSION="1.1.0"
 
 docker push $REPO_NAME/vic-machine-create:$VERSION
 docker push $REPO_NAME/vic-machine-debug:$VERSION
@@ -10,3 +10,18 @@ docker push $REPO_NAME/vic-machine-inspect:$VERSION
 docker push $REPO_NAME/vic-machine-ls:$VERSION
 docker push $REPO_NAME/vic-machine-rollback:$VERSION
 docker push $REPO_NAME/vic-machine-upgrade:$VERSION
+docker tag $REPO_NAME/vic-machine-create:$VERSION $REPO_NAME/vic-machine-create:latest
+docker tag $REPO_NAME/vic-machine-debug:$VERSION $REPO_NAME/vic-machine-debug:latest
+docker tag $REPO_NAME/vic-machine-delete:$VERSION $REPO_NAME/vic-machine-delete:latest
+docker tag $REPO_NAME/vic-machine-inspect:$VERSION $REPO_NAME/vic-machine-inspect:latest
+docker tag $REPO_NAME/vic-machine-ls:$VERSION $REPO_NAME/vic-machine-ls:latest
+docker tag $REPO_NAME/vic-machine-rollback:$VERSION $REPO_NAME/vic-machine-rollback:latest
+docker tag $REPO_NAME/vic-machine-upgrade:$VERSION $REPO_NAME/vic-machine-upgrade:latest
+docker push $REPO_NAME/vic-machine-create:latest
+docker push $REPO_NAME/vic-machine-debug:latest
+docker push $REPO_NAME/vic-machine-delete:latest
+docker push $REPO_NAME/vic-machine-inspect:latest
+docker push $REPO_NAME/vic-machine-ls:latest
+docker push $REPO_NAME/vic-machine-rollback:latest
+docker push $REPO_NAME/vic-machine-upgrade:latest
+
