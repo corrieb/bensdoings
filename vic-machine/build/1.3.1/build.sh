bb6ba0779ac8228b8d1f265adbcda8137df1e582
Ben Corrie <bcorrie@vmware.com>
2018-03-20 23:51:41 +0000

    Add files for 1.3.1

---
+++ vic-machine/build/1.3.1/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e 
+
+BUILD_FROM_OVA=false
+
+# build for pushing to dockerhub
+REPO_NAME="bensdoings"
+#REPO_NAME="vic-machine-local"
+
+# build for pushing to local registry
+# REPO_NAME="10.118.69.29/vic-machine"
+
+MAP_VERSION="1.2"
+VERSION="1.3.1"
+
+actions=( "create" "debug" "delete" "inspect" "ls" "rollback" "upgrade" "thumbprint" "firewall-allow" "firewall-deny" "dumpargs" "direct" )
+
+if [ "$BUILD_FROM_OVA" = true ]; then
+  cd ../../vic-machine-base/OVA
+else
+  cd ../../vic-machine-base/$VERSION
+fi
+
+docker build -t vic-machine-base .
+
+cd ../../actions/$MAP_VERSION
+cp ../Dockerfile* .
+
+for i in "${actions[@]}"
+do
+   docker build -f Dockerfile.$i -t $REPO_NAME/vic-machine-$i:$VERSION .
+done
+
+rm Dockerfile*
+
---
+++ vic-machine/build/1.3.1/push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# push to dockerhub
+REPO_NAME="bensdoings"
+
+# push to local registry
+#   note you'll need to create the project "vic-machine", add yourself to the project and run docker login with your credentials before pushing
+# REPO_NAME="10.118.69.29/vic-machine"
+
+VERSION="1.3.1"
+
+actions=( "create" "debug" "delete" "inspect" "ls" "rollback" "upgrade" "thumbprint" "firewall-allow" "firewall-deny" "dumpargs" "direct" )
+
+for i in "${actions[@]}"
+do
+   docker tag $REPO_NAME/vic-machine-$i:$VERSION $REPO_NAME/vic-machine-$i:latest
+   docker push $REPO_NAME/vic-machine-$i:$VERSION
+   docker push $REPO_NAME/vic-machine-$i:latest
+done
+
